5e99e20a60ca98ea479773c1fd41920707558a01
Guillermo Peralta Scura <gperaltascura@gmail.com>
2022-10-29 21:33:28 -0300

    Add more open graph thingies

--- src/components/router-head.tsx
+++ src/components/router-head.tsx
@@ -16,17 +16,48 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       <meta property="og:site_name" content="Guillermo Peralta Scura" />
+      <meta property="og:type" content="website" />
       <meta name="twitter:site" content="Guillermo Peralta Scura" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="scura.dev" />
+      <meta name="twitter:creator" content="@voluntadpear" />
+      <meta property="og:title" content={head.title} />
+      <meta name="twitter:title" content={head.title} />
 
       {head.meta.map((m) => (
         <meta {...m} />
       ))}
 
+      {!head.meta.some((m) => m.name === "description") ? (
+        <>
+          <meta
+            name="description"
+            content="Web developer that likes to tackle challenges, learn from them, write about them, and have fun while in the process!"
+          />
+          <meta
+            property="og:description"
+            content="Web developer that likes to tackle challenges, learn from them, write about them, and have fun while in the process!"
+          />
+          <meta
+            name="twitter:description"
+            content="Web developer that likes to tackle challenges, learn from them, write about them, and have fun while in the process!"
+          />
+        </>
+      ) : null}
+
       {!head.meta.some((m) => m.property === "og:image") ? (
-        <meta
-          property="og:image"
-          content={`https://${process.env.DOMAIN}/og-fallback.png`}
-        />
+        <>
+          <meta
+            property="og:image"
+            content={`https://${process.env.DOMAIN}/og-fallback.png`}
+          />
+          <meta
+            property="og:image:alt"
+            content="Guillermo Peralta Scura, front-end developer"
+          />
+          <meta property="og:image:width" content="1280" />
+          <meta property="og:image:height" content="675" />
+        </>
       ) : null}
 
       {!head.meta.some((m) => m.name === "twitter:image") ? (
